20d3715a0e17236fe548b7698a47a41c043ad09d
aashi gupta <aashigupta165@gmail.com>
2020-08-08 23:54:26 +0530

    navigation drawer UI

--- app/src/main/java/com/education/smartclass/AdminHomeActivity.java
+++ app/src/main/java/com/education/smartclass/AdminHomeActivity.java
@@ -1,24 +1,91 @@
 package com.education.smartclass;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.android.material.navigation.NavigationView;
+
 public class AdminHomeActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_admin_home);
+        setContentView(R.layout.nav_activity_main);
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         toolbar.setTitleTextColor(getResources().getColor(R.color.colorPrimaryDark));
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close);
+
+        drawerLayout.addDrawerListener(toggle);
+        toggle.getDrawerArrowDrawable().setColor(getResources().getColor(R.color.colorPrimaryDark));
+        toggle.syncState();
+
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.profile, R.id.teacher, R.id.classes, R.id.student)
+                .setDrawerLayout(drawerLayout)
+                .build();
+
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
+
+//        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+//            @Override
+//            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//                switch (item.getItemId()){
+//                    case R.id.profile:
+//                        Intent intent = new Intent(AdminHomeActivity.this, OtpActivity.class);
+//                        startActivity(intent);
+//                        return true;
+//                    case R.id.teacher:
+//                        intent = new Intent(AdminHomeActivity.this, ForgotPasswordActivity.class);
+//                        startActivity(intent);
+//                        return true;
+//                    case R.id.classes:
+//                        intent = new Intent(AdminHomeActivity.this, LoginActivity.class);
+//                        startActivity(intent);
+//                        return true;
+//                    case R.id.student:
+//                        intent = new Intent(AdminHomeActivity.this, OtpActivity.class);
+//                        startActivity(intent);
+//                        return true;
+//                }
+//                return true;
+//            }
+//        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }else{
+            super.onBackPressed();
+        }
     }
 
     @Override
